3737b3d51c37479165daaccc433d66bc99db3dfa
kavyabhuva <kavyabhuva8@gmail.com>
2026-01-06 10:24:12 -0800

    Add files via upload
    Main Screen

---
+++ Demo3/app/src/main/java/com/example/demo3/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.demo3; //
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.demo3.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void startGame(View v) { //
+        Intent intent = new Intent(HomeActivity.this, MainActivity.class); //
+        startActivity(intent); //
+    }
+}

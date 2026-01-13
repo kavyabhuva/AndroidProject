29c6515b564eb1a405e7cee4f81e32c190009ae6
devanshisojitra19-a11y <devanshisojitra19@gmail.com>
2026-01-13 21:36:21 +0530

    UI updates and logo changes

--- app/src/main/java/com/example/demo3/HomeActivity.java
+++ app/src/main/java/com/example/demo3/HomeActivity.java
@@ -3,12 +3,14 @@ import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.demo3.R;
+
 public class HomeActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
 
     }
 }
--- app/src/main/java/com/example/demo3/SplashActivity.java
+++ app/src/main/java/com/example/demo3/SplashActivity.java
@@ -1,14 +1,20 @@
+package com.example.demo3;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(() -> {
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, HomeActivity.class));
             finish();
         }, 3000);
